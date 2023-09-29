0c1f72d2378f2cae783732ca468c210fc84e91f7
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-09-29 15:58:18 +0530

    Update provider.tf

--- Iac/provider.tf
+++ Iac/provider.tf
@@ -2,7 +2,7 @@
 provider "azurerm" {
   subscription_id = "85cd2292-82e3-4c72-a2d7-1ba724a25176"
   tenant_id = "120709b9-1cde-4a68-944a-f6fb5b566111"
-  client_id = "80519e20-919b-4d81-a8d9-405d697d2644"
+  client_id = "a53107bf-fbf5-40cf-8752-e556704b2c67"
   client_secret  = var.client_secret
   features {}
 }
